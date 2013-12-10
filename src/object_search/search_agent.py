fba4bd74dc41c8a44a80f8ef4d09dce4e0b13e94
Lars Kunze <kula78@gmail.com>
2013-12-10 08:32:18 +0000

    added ptu behaviour to simulation

--- src/object_search/search_agent.py
+++ src/object_search/search_agent.py
@@ -314,7 +314,8 @@ class PerceiveSim (smach.State):
                              output_keys=['obj_list'])
 
         rospy.Subscriber("semcam", String, self.callback)
-
+        self.ptu_cmd = rospy.Publisher('/ptu/cmd', JointState)
+        
         self.obj_list = []
         self.active = False
         self.first_call = False
@@ -328,20 +329,69 @@ class PerceiveSim (smach.State):
             for obj_desc in obj_list:
                 rospy.loginfo("Perceived: %s" % obj_desc.get('name'))
 
-            self.obj_list = obj_list
+            for obj in obj_list:
+                self.obj_list.append(obj)
 
     def execute(self, userdata):
-        rospy.loginfo('Executing state %s', self.__class__.__name__)
-        self.active = True
-        self.first_call = True
+        # rospy.loginfo('Executing state %s', self.__class__.__name__)
+        # self.active = True
+        # self.first_call = True
 
-        # wait for some time to read from the topic
-        # TODO: replace with a service call
-        rospy.sleep(3)
+        # # wait for some time to read from the topic
+        # # TODO: replace with a service call
+        # rospy.sleep(3)
+        # userdata.obj_list = self.obj_list
+
+        # self.active = False
+        # return 'succeeded'
+        rospy.loginfo('Executing state %s', self.__class__.__name__)
+
+        self.obj_list = []
+
+        i = 1
+        for view in userdata.view_list:
+
+            rospy.loginfo('%i view: set PTU to %s',i,view)
+
+            joint_state = JointState()
+            joint_state.header.frame_id = 'tessdaf'
+            joint_state.name = ['pan', 'tilt']
+            joint_state.position = [float(view[0]),float(view[1])]
+            joint_state.velocity = [float(1.0),float(1.0)]
+            joint_state.effort = [float(1.0),float(1.0)]
+            
+            self.ptu_cmd.publish(joint_state)
+            
+            # wait until PTU has finished or point cloud get screwed up
+            rospy.sleep(2)
+            
+            rospy.loginfo('%i view: receive from semantic camera',i)
+            self.active = True
+            self.first_call = True
+
+            # wait for some time to read once from the topic and store it onto self.pointcloud
+            # TODO: replace with a service call
+            rospy.sleep(2)
+            
+            self.active = False
+
+            i = i + 1
+
+
         userdata.obj_list = self.obj_list
 
-        self.active = False
+        # back to original position
+        joint_state = JointState()
+        joint_state.header.frame_id = 'tessdaf'
+        joint_state.name = ['pan', 'tilt']
+        joint_state.position = [float(0.0),float(0.0)]
+        joint_state.velocity = [float(1.0),float(1.0)]
+        joint_state.effort = [float(1.0),float(1.0)]
+            
+        self.ptu_cmd.publish(joint_state)
+
         return 'succeeded'
+
 
 
 class PerceiveReal (smach.State):
